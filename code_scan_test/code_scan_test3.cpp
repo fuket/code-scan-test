845f378b86f65868397a867a7abad669ab9701f9
Tomoki Fuke <earth-pathfinder@live.jp>
2025-07-16 23:27:55 +0900

    add file

---
+++ code_scan_test/code_scan_test3.cpp
@@ -0,0 +1,17 @@
+﻿// code_scan_test.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+
+int main()
+{
+	char buf[] = "Hello World!"; // 文字列を格納するバッファ
+	for (int i = 0; i < 30; ++i)
+	{
+		printf("%c", buf[i]); // バッファの内容を1文字ずつ出力
+	}
+	printf("\n");
+
+	return 0;
+}
+
---
+++ code_scan_test/code_scan_test4.cpp
@@ -0,0 +1,18 @@
+﻿// code_scan_test.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+
+int&& add(const int a, const int b)
+{
+	int result = a + b;
+	return std::move(result);
+}
+
+int main()
+{
+	printf("%d\b", add(1, 2));
+
+	return 0;
+}
+
