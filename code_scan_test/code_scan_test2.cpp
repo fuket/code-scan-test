90a72413bfe9131f7bc4accef61ffc86f1d17acf
Tomoki Fuke <earth-pathfinder@live.jp>
2025-07-16 23:19:32 +0900

    add file

---
+++ code_scan_test/code_scan_test2.cpp
@@ -0,0 +1,26 @@
+﻿// code_scan_test.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+
+int getlength(char buf[])
+{
+	//めちゃくちゃ
+	int size = sizeof(buf);
+	return size;
+}
+
+int main()
+{
+	char buf[] = "Hello World!"; // 文字列を格納するバッファ
+	for (int i = 0; i < 30; ++i)
+	{
+		printf("%c", buf[i]); // バッファの内容を1文字ずつ出力
+	}
+	printf("\n");
+
+	printf("size: %d, %s\n", getlength(buf));
+
+	return 0;
+}
+
