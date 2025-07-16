f3da987cd6de043f65622b43367ec77db2e6ffeb
Tomoki Fuke <earth-pathfinder@live.jp>
2025-07-16 22:24:23 +0900

    initial commit

---
+++ code_scan_test/code_scan_test.cpp
@@ -0,0 +1,16 @@
+﻿// code_scan_test.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#include <iostream>
+
+int main()
+{
+	char buf[] = "Hello World!"; // 文字列を格納するバッファ
+	for (int i = 0; i < sizeof(buf) + 1; ++i)
+	{
+		printf("%c", buf[i]); // バッファの内容を1文字ずつ出力
+	}
+	printf("\n");
+    return 0;
+}
+
